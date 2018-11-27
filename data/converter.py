6687b2fae6f8b5ef7594d2b18ec25d845d08a56d
Ujwal Narayan <ujwalnarayann@gmail.com>
2018-11-27 22:42:44 +0530

    Fixed punct and tag issues

---
+++ data/converter.py
@@ -0,0 +1,11 @@
+lines = open("agreement/"+'train.txt' ,).\
+        read().strip().split('\n')
+inp = []
+out = []
+for line in lines :
+    sent = line.split("\t")
+    inp.append(sent[0])
+    out.append(sent[1])
+f = open("agreement/"+'train.txt' ,"w")
+for index in range(len(inp)):
+    f.write(out[index] + "\t" + inp[index] + "\n")
